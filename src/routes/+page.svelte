ed1fe99b6adaf245539acc0efca895cab9d6519c
Aman Varshney <av.amanvarshney11@gmail.com>
2022-10-26 01:01:48 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="font-bold text-center">Welcome to SvelteKit {name}</h1>
+<p class="text-3xl underline">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang='ts'>
+    let name: Number = 7 + 6;
+
+</script>
+
