92736342f3f7521312efa8ddb4adeb2d9bd6d477
Aman Varshney <av.amanvarshney11@gmail.com>
2022-10-26 20:19:06 +0530

    api not working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,11 @@
 
 	const apiKey = '2c710afd'
 	let data: any;
-	let data2: any;
 	let movie: any = {};
-	let copy: any = {};
 
 	async function getDetails (movieName: string) {
 		data = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${movieName}`).then((x) => x.json());
 		movie = {...data}
-
-		data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then((x) => x.json());
-		copy = {...data}
 	};
 
 	getDetails('Top_Gun_Maverick')
@@ -19,7 +14,6 @@
 </script>
 
 <pre>
-	{copy.title}
 	{JSON.stringify(movie)}
   {movie.Title}
 </pre>
