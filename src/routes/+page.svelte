c5ff24cb99b8273433e1bba72d040acf107cb7b3
Aman Varshney <av.amanvarshney11@gmail.com>
2022-10-27 21:40:19 +0530

    updated css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,25 +4,26 @@
 
 	const apiKey = '2c710afd'
 	let data: any;
-	let movie: any = {};
+	let movie: any;
 
 	async function getDetails () {
-		let randomMovie = movies[Math.floor(Math.random()*movies.length)]
+		let randomMovie: string = movies[Math.floor(Math.random()*movies.length)]
 		data = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${randomMovie}`).then((x) => x.json());
 		movie = {...data}
 	};
-
-	// getDetails('Breaking_Bad')
+	
 </script>
 
-<div class="">
-	<h1><button on:click={getDetails}>Entertain Me Aman</button></h1>
 
+<div class="h-screen w-full flex flex-col-reverse justify-center items-center">
+	<button on:click={getDetails}>Entertain Me Aman</button>
+	{#if movie}
 	<p>
 		{JSON.stringify(movie)}
-  		{movie.Title}
+		{movie.Title}
 	</p>
 	<img class="h-1/4 w-auto" src="{movie.Poster}" alt="{movie.Title} Poster">
+	{/if}
 </div>
 
   
