20f023236c967c1385258ad72d2fe3b42a369bf2
Aman Varshney <av.amanvarshney11@gmail.com>
2022-10-29 00:13:31 +0530

    added moviedb api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 	import {movies, shows, animatedMovies, games, animes} from "../data.json";
 
-	const apiKeyOmdb = '2c710afd'
+	const apiKeyTmdb = '0f30077ee9f1be6f1d82b08eb555e7af'
 	const apiKeyRawg = 'cad2990e2205456cadc2e24ab207f45a'
 	// let data: any;
 	let movie: any
@@ -13,19 +13,19 @@
 
 	async function getMovieDetails () {
 		let random: string = movies[Math.floor(Math.random()*movies.length)]
-		let data: any = await fetch(`https://www.omdbapi.com/?apikey=${apiKeyOmdb}&t=${random}`).then((x) => x.json());
+			let data: any = await fetch(`https://api.themoviedb.org/3/movie/${random}?api_key=${apiKeyTmdb}`).then((x) => x.json());
 		movie = {...data}
 	};
 	
-	async function getGamesDetails () {
+	async function getGameDetails () {
 		let random: string = games[Math.floor(Math.random() * games.length)]
 		let data: any = await fetch(`https://rawg.io/api/games/${random}?key=${apiKeyRawg}`).then((x) => x.json())
 		game = {...data}
 	}
 
-	async function getShowsDetails () {
+	async function getShowDetails () {
 		let random: string = shows[Math.floor(Math.random() * shows.length)]
-		let data: any = await fetch(`https://www.omdbapi.com/?apikey=${apiKeyOmdb}&t=${random}`).then((x) => x.json())
+		let data: any = await fetch(`https://api.themoviedb.org/3/movie/${random}?api_key=${apiKeyTmdb}`).then((x) => x.json())
 		show = {...data}
 	}
 
@@ -40,11 +40,11 @@
 		if (currentSelected == 'movies') {
 			getMovieDetails()
 		} else if (currentSelected == 'games') {
-			getGamesDetails()
+			getGameDetails()
 		} else if (currentSelected == 'shows') {
-			getShowsDetails()
+			getShowDetails()
 		} else {
-			getShowsDetails()
+			getMovieDetails()
 		}
 	}
 	
@@ -53,23 +53,14 @@
 </script>
 
 
-<div class="min-h-screen flex flex-col-reverse justify-center items-center">
-
-	<button on:click={getDetails}>Entertain Me Aman</button>
-
-	<div class="flex justify-between">
-		<button on:click={() => {currentSelected = 'movies'}}>Movies</button>
-		<button on:click={() => {currentSelected = 'shows'}}>Shows</button>
-		<button on:click={() => {currentSelected = 'amovies'}}>Animated Movies</button>
-		<button on:click={() => {currentSelected = 'anime'}}>Anime</button>
-		<button on:click={() => {currentSelected = 'games'}}>Games</button>
-	</div>
+<div class="min-h-screen flex flex-col justify-center items-center">
 
 	{#if movie}
 	<div>
-			<!-- {JSON.stringify(movie)} -->
-		<img class="h-1/4 w-auto" src="{movie.Poster}" alt="{movie.Title} Poster">
-		<h1>{movie.Title}</h1>
+	
+		<h1>{movie.title}</h1>
+		<img class="w-auto h-3/5" src="https://image.tmdb.org/t/p/w342/{movie.poster_path}" alt="{movie.Title} Poster">
+
 
 	</div>
 	{/if}
@@ -77,12 +68,30 @@
 	{#if show}
 	<div>
 	
-		<img class="h-1/4 w-auto" src="{show.Poster}" alt="{show.Title} Poster">
+		<img class="w-auto h-3/5" src="{show.Poster}" alt="{show.Title} Poster">
 		<h1>{show.Title}</h1>
 
 	</div>
 	{/if}
 
+	{#if game}
+		<div>
+
+			{game.name}
+			<img class="w-96" src="{game.background_image}" alt="">
+
+		</div>
+	{/if}
+
+	<button on:click={getDetails}>Entertain Me Aman</button>
+
+	<div class="">
+		<button on:click={() => {currentSelected = 'movies'}}>Movies</button>
+		<button on:click={() => {currentSelected = 'shows'}}>Shows</button>
+		<button on:click={() => {currentSelected = 'amovies'}}>Animated Movies</button>
+		<button on:click={() => {currentSelected = 'anime'}}>Anime</button>
+		<button on:click={() => {currentSelected = 'games'}}>Games</button>
+	</div>
 
 </div>
 
